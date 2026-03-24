b2dee43fe82a84e78b1c50c744677313d7823df5
HjalmarRantala <hjalmar.jr@gmail.com>
2026-03-24 19:42:47 +0100

    Adding the foundation of the sops-nix secrets handling

--- flake.nix
+++ flake.nix
@@ -3,17 +3,20 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
-    
+    sops-nix = {
+      url = "github:Mic92/sops-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
-    
+  outputs = { self, nixpkgs, sops-nix, ... }@inputs: {
+
     nixosConfigurations = {
-      
       p52s = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };
         modules = [
+          sops-nix.nixosModules.sops
           ./hosts/p52s/configuration.nix
         ];
       };
