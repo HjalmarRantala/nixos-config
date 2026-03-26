d82c879e25eeee4d123bb7af3720cf4f1078f3e6
HjalmarRantala <hjalmar.jr@gmail.com>
2026-03-26 15:35:18 +0100

    Fixing automatic deployment

--- common/deployment.nix
+++ common/deployment.nix
@@ -3,10 +3,13 @@
 {
   system.autoUpgrade = {
     enable = true;
-    flake = "git+https://codeberg.org/danskjavel/nixos-config.git#${config.networking.hostName}";
+    flake = "git+ssh://git@codeberg.org/danskjavel/nixos-config.git#${config.networking.hostName}";
 
     dates = "*:0/3";
 
     randomizedDelaySec = "20s";
+  };
+  systemd.services.nixos-upgrade.environment = {
+      GIT_SSH_COMMAND = "ssh -i /run/secrets/deploy_key -o IdentitiesOnly=yes";
   };
 }
