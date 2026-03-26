2f7582cfad3ca16ee64d308395eb9a3375d6f648
HjalmarRantala <hjalmar.jr@gmail.com>
2026-03-26 14:56:44 +0100

    Typo

--- common/deployment.nix
+++ common/deployment.nix
@@ -3,10 +3,10 @@
 {
   system.autoUpgrade = {
     enable = true;
-    flake = "git+https://codeberg.org/ditt-användarnamn/nixos-config.git#p52s"; 
-    
-    dates = "*:0/3"; 
-    
-    randomizedDelaySec = "20s"; 
+    flake = "git+https://codeberg.org/danskjavel/nixos-config.git#${config.networking.hostName}";
+
+    dates = "*:0/3";
+
+    randomizedDelaySec = "20s";
   };
 }
