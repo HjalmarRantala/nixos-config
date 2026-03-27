c9d22f81c68db808a03ffbf84ebf43324f09ea39
HjalmarRantala <hjalmar.jr@gmail.com>
2026-03-27 11:26:15 +0100

    Adding monitoring using grafana and prometheus

---
+++ common/monitor.nix
@@ -0,0 +1,31 @@
+{ config, lib, pkgs, ... }:
+
+{
+services.prometheus = {
+    enable = true;
+    port = 9090;
+    
+    exporters.node = {
+      enable = true;
+      port = 9100;
+      enabledCollectors = [ "systemd" ];
+    };
+    
+    scrapeConfigs = [
+      {
+        job_name = "p52s-system";
+        static_configs = [{ targets = [ "127.0.0.1:9100" ]; }];
+      }
+    ];
+  };
+
+  services.grafana = {
+    enable = true;
+    settings = {
+      server = {
+        http_port = 3000;
+        http_addr = "0.0.0.0"; 
+      };
+    };
+  };
+}
