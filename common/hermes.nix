dd914400cff204dadcb3ad36bcfa25294e8a45af
HjalmarRantala <hjalmar.jr@gmail.com>
2026-05-27 11:22:08 +0200

    Testing non-docker setup of hermes

--- common/hermes.nix
+++ common/hermes.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   services.hermes-agent = {
@@ -8,6 +8,9 @@
     environmentFiles = [
       config.sops.secrets.hermes_env.path
     ];
+
+    stateDir = "/home/hermes-runner/.hermes";
+    workingDirectory = "/home/hermes-runner/workspace";
 
     container = {
       enable = true;
@@ -19,14 +22,41 @@
         default = "gpt-5.4";
         provider = "openai-codex";
       };
+      terminal.backend = "local";
       providers.openai-codex.enabled = true;
     };
   };
-  security.sudo.extraRules = [{
-    users = [ "hjalmar" ];
-    commands = [{
-      command = "/run/current-system/sw/bin/docker";
-      options = [ "NOPASSWD" ];
-    }];
-  }];
+
+#  security.sudo.extraRules = [{
+#    users = [ "hjalmar" ];
+#    commands = [{
+#      command = "/run/current-system/sw/bin/docker";
+#      options = [ "NOPASSWD" ];
+#    }];
+#  }];
+
+  users.users.hermes-runner = {
+    isNormalUser = true;
+    description = "Hermes Agent Runner";
+    packages = with pkgs; [
+      git
+      gh
+      codeberg-cli
+      openssh
+      nixfmt-rfc-style
+      jq
+      curl
+      statix
+    ];
+    openssh.authorizedKeys.keys = [];
+  };
+
+  systemd.services.hermes-agent.serviceConfig = {
+    User = lib.mkForce "hermes-runner";
+    Group = lib.mkForce "users";
+    ProtectHome = lib.mkForce "read-only"; 
+    ReadWritePaths = [ 
+      "/home/hermes-runner" 
+    ];
+  };
 }
